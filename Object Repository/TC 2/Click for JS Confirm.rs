2ef35e2d1accc88b0278f627485acad1e1b3e28a
Mehedi Amin <MehediAmin90@gmail.com>
2021-01-25 22:20:10 -0500

    Dismiss Alert TC

---
+++ Object Repository/TC 2/Click for JS Confirm.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click for JS Confirm</name>
+   <tag></tag>
+   <elementGuidId>fd9ebf7b-8934-4aff-9eb3-b188546b7a09</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@onclick='jsConfirm()']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//button[@onclick='jsConfirm()']</value>
+   </webElementProperties>
+</WebElementEntity>
