3b252c9cec0c1e21e4d9c0cc9811a474315e9fd6
Mehedi Amin <MehediAmin90@gmail.com>
2021-01-25 21:49:51 -0500

    Handle Alert Pop-Up

---
+++ Object Repository/TC 2/Click for JS Alert.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Click for JS Alert</name>
+   <tag></tag>
+   <elementGuidId>be3854f4-0c99-4694-af3a-6f800cfc1f71</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@onclick='jsAlert()']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//button[@onclick='jsAlert()']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/TC 2/JavaScript Alerts.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>JavaScript Alerts</name>
+   <tag></tag>
+   <elementGuidId>8b64997f-5302-4257-8b03-f5d1587a2d24</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[contains(text(),'JavaScript Alerts')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'JavaScript Alerts')]</value>
+   </webElementProperties>
+</WebElementEntity>
